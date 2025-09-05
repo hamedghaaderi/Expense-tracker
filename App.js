6d66c98faffa30a28f93df373633a2e4fc039bda
Hamed Ghaderi <hamedghaderi.dev@gmail.com>
2025-09-05 16:32:00 +0330

    feat: add custom navigation styles. also add custom font family

--- App.js
+++ App.js
@@ -5,27 +5,71 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import ManageExpensesScreen from "./screens/manage-expenses-screen";
 import AllExpensesScreen from "./screens/all-expenses-screen";
 import RecentExpensesScreen from "./screens/recent-expenses-screen";
+import GlobalStyles from "./constants/colors";
+import { Ionicons } from "@expo/vector-icons";
+import { useFonts } from "expo-font";
 
 const Stack = createNativeStackNavigator();
 const BottomTabs = createBottomTabNavigator();
 
 const OtherScreens = () => {
   return (
-    <BottomTabs.Navigator>
-      <BottomTabs.Screen name="AllScreen" component={AllExpensesScreen} />
-      <BottomTabs.Screen name="RecentScreen" component={RecentExpensesScreen} />
+    <BottomTabs.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: GlobalStyles.colors.primary500 },
+        headerTintColor: "white",
+        tabBarStyle: { backgroundColor: GlobalStyles.colors.primary500 },
+        tabBarActiveTintColor: GlobalStyles.colors.accent500,
+        tabBarLabelStyle: { fontFamily: "samim", fontSize: 15 },
+        tabBarLabelPosition: "beside-icon",
+      }}
+    >
+      <BottomTabs.Screen
+        name="AllScreen"
+        component={AllExpensesScreen}
+        options={{
+          title: "همه مخارج",
+          tabBarIcon: ({ size, color }) => {
+            return <Ionicons name="calendar" size={size} color={color} />;
+          },
+        }}
+      />
+      <BottomTabs.Screen
+        name="RecentScreen"
+        component={RecentExpensesScreen}
+        options={{
+          title: "مخارج اخیر",
+          tabBarIcon: ({ size, color }) => (
+            <Ionicons name="hourglass" size={size} color={color} />
+          ),
+        }}
+      />
     </BottomTabs.Navigator>
   );
 };
 
 export default function App() {
+  useFonts({
+    samim: require("./assets/fonts/Samim.ttf"),
+  });
+
   return (
     <>
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
       <NavigationContainer>
         <Stack.Navigator>
-          <Stack.Screen name="OtherScreens" component={OtherScreens} />
-          <Stack.Screen name="ManageScreen" component={ManageExpensesScreen} />
+          <Stack.Screen
+            name="OtherScreens"
+            component={OtherScreens}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="ManageScreen"
+            component={ManageExpensesScreen}
+            options={{
+              titleL: "مدیریت مخارج",
+            }}
+          />
         </Stack.Navigator>
       </NavigationContainer>
     </>
